24ff55c76615eae7e408c797f590ed169db65d82
Sandeep-Kumawat <sandeepkumawatsa77@gmail.com>
2021-05-05 12:58:14 +0530

    CRUD Api

---
+++ Crud Api/CRUD Api/CRUD Api/Infrastructure/ToDoItemContext.cs
@@ -0,0 +1,19 @@
+﻿using CRUD_Api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_Api.Infrastructure
+{
+    public class ToDoItemContext : DbContext
+    {
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+
+        public ToDoItemContext(DbContextOptions<ToDoItemContext> options):base(options)
+        {
+
+        }
+    }
+}
