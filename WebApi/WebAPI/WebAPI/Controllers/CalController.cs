b183147f45078af3bdd4ca5e72a1b721c3bb2cba
Sandeep-Kumawat <sandeepkumawatsa77@gmail.com>
2021-05-05 13:00:50 +0530

    Web Api

---
+++ WebApi/WebAPI/WebAPI/Controllers/CalController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalController : ControllerBase
+    {
+        [HttpGet("greet")]
+        public string greet()
+        {
+            return "Hello Sandeep";
+        }
+        [HttpGet("products")]
+        public List<Product> products()
+        {
+            var p = new List<Product>
+            {
+                new Product
+                {
+                    Name="Pen",
+                    Price=20,
+                    Quantity=5
+                },
+                 new Product
+                {
+                    Name="Pencil",
+                    Price=10,
+                    Quantity=3
+                }
+            };
+            return p;
+        }
+        [HttpPost("Calculate")]
+        public int cal(Operation op)
+        {
+            int res = 0;
+            if(op.Operands == "+")
+            {
+                res = op.num1 + op.num2;
+            }
+            else if (op.Operands == "-")
+            {
+                res = op.num1 - op.num2;
+            }
+            else if (op.Operands == "*")
+            {
+                res = op.num1 * op.num2;
+            }
+            if (op.Operands == "+")
+            {
+                res = op.num1 + op.num2;
+            }
+            else if (op.Operands == "-")
+            {
+                res = op.num1 - op.num2;
+            }
+            else if (op.Operands == "*")
+            {
+                res = op.num1 * op.num2;
+            }
+            else if (op.Operands == "/") 
+            {
+                res = op.num1 / op.num2;
+            }
+            else
+            {
+                return -1;
+            }
+            return res;
+        }
+    }
+}
---
+++ WebApi/WebAPI/WebAPI/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class Operation
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        public string Operands { get; set; }
+    }
+}
