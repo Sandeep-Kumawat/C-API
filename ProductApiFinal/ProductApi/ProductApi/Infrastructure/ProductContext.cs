5c7912262f83017417d745ae2026c886c93530ef
Sandeep-Kumawat <sandeepkumawatsa77@gmail.com>
2021-05-05 13:00:05 +0530

    Product Api Final

---
+++ ProductApiFinal/ProductApi/ProductApi/Infrastructure/ProductContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApi.Infrastructure
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> products { get; set; }
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+
+        }
+    }
+}
